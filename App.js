f39a62b0ed4f5cc462360a16b2381ea386286aca
Thibault Durand <tibo.durand@gmail.com>
2018-07-14 11:21:39 +0200

    Evo: minimal working example

--- App.js
+++ App.js
@@ -1,29 +1,159 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { StyleSheet, View, Alert } from 'react-native';
+import MapView from 'react-native-maps';
+// import BackgroundGeolocation from 'react-native-mauron85-background-geolocation';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+export default class App extends Component {
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+  constructor(props) {
+    super(props);
 
-type Props = {};
-export default class App extends Component<Props> {
+    this.state = {
+      trackingHistory: [],
+      currentPosition: null
+    }
+  }
+
+  // initBackgroundGeolocation() {
+  //   BackgroundGeolocation.configure({
+  //     desiredAccuracy: BackgroundGeolocation.HIGH_ACCURACY,
+  //     stationaryRadius: 1,
+  //     distanceFilter: 1,
+  //     notificationTitle: 'Mouillage app location tracking',
+  //     notificationText: 'enabled',
+  //     debug: true,
+  //     startOnBoot: false,
+  //     stopOnTerminate: false,
+  //     locationProvider: BackgroundGeolocation.ACTIVITY_PROVIDER,
+  //     interval: 5000,
+  //     fastestInterval: 5000,
+  //     activitiesInterval: 5000,
+  //     stopOnStillActivity: false
+  //   });
+
+  //   BackgroundGeolocation.on('location', (location) => {
+  //     // handle your locations here
+  //     // to perform long running operation on iOS
+  //     // you need to create background task
+  //     const lnglat = [location.longitude, location.latitude]
+
+  //     // TODO if location.accuracy > ?? m blabla
+  //     // END TODO
+  //     console.log(location);
+
+  //     this.setState({
+  //       trackingHistory: [...this.state.trackingHistory, lnglat],
+  //       currentPosition: lnglat
+  //     })
+
+  //     // BackgroundGeolocation.startTask(taskKey => {
+  //     //   // execute long running task
+  //     //   // eg. ajax post location
+  //     //   // IMPORTANT: task has to be ended by endTask
+  //     //   BackgroundGeolocation.endTask(taskKey);
+  //     // });
+  //   });
+
+  //   BackgroundGeolocation.on('stationary', (stationaryLocation) => {
+  //     // handle stationary locations here
+  //     console.log(stationaryLocation);
+  //   });
+
+  //   BackgroundGeolocation.on('error', (error) => {
+  //     console.log('[ERROR] BackgroundGeolocation error:', error);
+  //   });
+
+  //   BackgroundGeolocation.on('start', () => {
+  //     console.log('[INFO] BackgroundGeolocation service has been started');
+  //   });
+
+  //   BackgroundGeolocation.on('stop', () => {
+  //     console.log('[INFO] BackgroundGeolocation service has been stopped');
+  //   });
+
+  //   BackgroundGeolocation.on('authorization', (status) => {
+  //     console.log('[INFO] BackgroundGeolocation authorization status: ' + status);
+  //     if (status !== BackgroundGeolocation.AUTHORIZED) {
+  //       // we need to set delay or otherwise alert may not be shown
+  //       setTimeout(() =>
+  //         Alert.alert('App requires location tracking permission', 'Would you like to open app settings?', [
+  //           { text: 'Yes', onPress: () => BackgroundGeolocation.showAppSettings() },
+  //           { text: 'No', onPress: () => console.log('No Pressed'), style: 'cancel' }
+  //         ]), 1000);
+  //     }
+  //   });
+
+  //   BackgroundGeolocation.on('background', () => {
+  //     console.log('[INFO] App is in background');
+  //   });
+
+  //   BackgroundGeolocation.on('foreground', () => {
+  //     console.log('[INFO] App is in foreground');
+  //   });
+
+  //   BackgroundGeolocation.checkStatus(status => {
+  //     console.log('[INFO] BackgroundGeolocation service is running', status.isRunning);
+  //     console.log('[INFO] BackgroundGeolocation services enabled', status.locationServicesEnabled);
+  //     console.log('[INFO] BackgroundGeolocation auth status: ' + status.authorization);
+
+  //     // you don't need to check status before start (this is just the example)
+  //     if (!status.isRunning) {
+  //       BackgroundGeolocation.start(); //triggers start on start event
+  //     }
+
+  //     // you can also just start without checking for status
+  //     // BackgroundGeolocation.start();
+  //   });
+
+  // }
+
+  componentDidMount() {
+    // this.initBackgroundGeolocation();
+  }
+
+  componentWillUnmount() {
+    // unregister all event listeners
+    // BackgroundGeolocation.events.forEach(event => BackgroundGeolocation.removeAllListeners(event));
+  }
+
+  renderTrackingHistory () {
+
+    // if(this.state.trackingHistory.length < 2) {
+    //   return;
+    // }
+
+    // var geojsonLine = { "type": "LineString", "coordinates": this.state.trackingHistory }
+
+    // return (
+    //   <Mapbox.ShapeSource 
+    //     id='trackingHistory' 
+    //     shape={geojsonLine}
+    //   >
+    //     <Mapbox.LineLayer id='trackingHistoryFill' style={layerStyles.trackingHistory} />
+    //   </Mapbox.ShapeSource>
+    // )
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        {/* <Mapbox.MapView
+            styleURL={Mapbox.StyleURL.Light}
+            zoomLevel={17}
+            centerCoordinate={this.state.currentPosition}
+            style={styles.container}> 
+            {this.renderCircle()}
+            {this.renderTrackingHistory()}
+        </Mapbox.MapView> */}
+        <MapView
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          style={styles.map}
+        />
       </View>
     );
   }
@@ -31,19 +161,11 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  }
 });
