26a53fa9ae96f1105f3af6b2a394c25f5841a94e
Thibault Durand <tibo.durand@gmail.com>
2018-07-16 11:22:43 +0200

    WIP: playing with markers

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Alert } from 'react-native';
-import MapView, { Polyline } from 'react-native-maps';
+import { StyleSheet, View, Alert, Animated } from 'react-native';
+import MapView, { Polyline, Circle, Marker } from 'react-native-maps';
 import BackgroundGeolocation from 'react-native-mauron85-background-geolocation';
+
+import boatIcon from './assets/boat.png';
+import anchorIcon from './assets/anchor2.png';
+
+const style = {
+  width: 30,
+  height: 43.8,
+  transform: [
+    { rotate: `30deg` }
+  ]
+}
 
 export default class App extends Component {
 
@@ -145,19 +156,48 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <MapView
-          region={{
-            latitude: this.state.currentPosition.latitude || 37.78825,
-            longitude: this.state.currentPosition.longitude || -122.4324,
-            latitudeDelta: 0.005,
-            longitudeDelta: 0.005,
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.001,
+            longitudeDelta: 0.001,
           }}
+          showsCompass={true}
           style={styles.map}
         >
-          <Polyline
+          {/* <Polyline
             coordinates={this.state.trackingHistory}
             strokeColor="#000"
             strokeWidth={5}
+          /> */}
+          <Circle 
+            radius={30}
+            strokeWidth={3}
+            strokeColor="red"
+            center={{
+              latitude: 37.78825,
+              longitude: -122.4324
+            }}
           />
+          {/* <Marker 
+            title="Boat"
+            image={boatIcon}
+            coordinate={{
+              latitude: 37.78825,
+              longitude: -122.4324
+            }}
+          /> */}
+          <MapView.Marker
+            key='anchor-marker'
+            anchor={{ x: 0.5, y: 0.5 }}
+            // zIndex={this.props.zIndex}
+            coordinate={{
+              latitude: 37.78825,
+              longitude: -122.4324
+            }}
+          >
+            <Animated.Image ref='image' style={style} source={anchorIcon} />
+          </MapView.Marker>
         </MapView>
       </View>
     );
